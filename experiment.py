03cd1c89dcf815d66fb1bce2ff16b22ccc039f42
zhiyuanyou <zhiyuanyou@foxmail.com>
2023-05-12 21:25:01 +0800

    feat(./): add new optim, revise style & para

--- experiment.py
+++ experiment.py
@@ -1,22 +1,35 @@
 import os
-import math
-import torch
 from torch import optim
 from models import BaseVAE
 from models.types_ import *
-from utils import data_loader
 import pytorch_lightning as pl
-from torchvision import transforms
 import torchvision.utils as vutils
-from torchvision.datasets import CelebA
-from torch.utils.data import DataLoader
+
+
+def get_optimizer(parameters, config):
+    if config['type'] == "AdamW":
+        return optim.AdamW(parameters, **config['kwargs'])
+    elif config['type'] == "Adam":
+        return optim.Adam(parameters, **config['kwargs'])
+    elif config['type'] == "SGD":
+        return optim.SGD(parameters, **config['kwargs'])
+    else:
+        raise NotImplementedError
+
+
+def get_scheduler(optimizer, config):
+    if config['type'] == "MultiStepLR":
+        return optim.lr_scheduler.MultiStepLR(optimizer, **config['kwargs'])
+    elif config['type'] == "StepLR":
+        return optim.lr_scheduler.StepLR(optimizer, **config['kwargs'])
+    elif config['type'] == "ExponentialLR":
+        return optim.lr_scheduler.ExponentialLR(optimizer, **config['kwargs'])
+    else:
+        raise NotImplementedError
 
 
 class VAEXperiment(pl.LightningModule):
-
-    def __init__(self,
-                 vae_model: BaseVAE,
-                 params: dict) -> None:
+    def __init__(self, vae_model: BaseVAE, params: dict) -> None:
         super(VAEXperiment, self).__init__()
 
         self.model = vae_model
@@ -31,61 +44,71 @@ class VAEXperiment(pl.LightningModule):
     def forward(self, input: Tensor, **kwargs) -> Tensor:
         return self.model(input, **kwargs)
 
-    def training_step(self, batch, batch_idx, optimizer_idx = 0):
+    def training_step(self, batch, batch_idx, optimizer_idx=0):
         real_img, labels = batch
         self.curr_device = real_img.device
 
-        results = self.forward(real_img, labels = labels)
-        train_loss = self.model.loss_function(*results,
-                                              M_N = self.params['kld_weight'], #al_img.shape[0]/ self.num_train_imgs,
-                                              optimizer_idx=optimizer_idx,
-                                              batch_idx = batch_idx)
+        results = self.forward(real_img, labels=labels)
+        train_loss = self.model.loss_function(
+            *results,
+            M_N=self.
+            params['kld_weight'],  #al_img.shape[0]/ self.num_train_imgs,
+            optimizer_idx=optimizer_idx,
+            batch_idx=batch_idx)
 
-        self.log_dict({key: val.item() for key, val in train_loss.items()}, sync_dist=True)
+        self.log_dict({key: val.item()
+                       for key, val in train_loss.items()},
+                      sync_dist=True)
 
         return train_loss['loss']
 
-    def validation_step(self, batch, batch_idx, optimizer_idx = 0):
+    def validation_step(self, batch, batch_idx, optimizer_idx=0):
         real_img, labels = batch
         self.curr_device = real_img.device
 
-        results = self.forward(real_img, labels = labels)
-        val_loss = self.model.loss_function(*results,
-                                            M_N = 1.0, #real_img.shape[0]/ self.num_val_imgs,
-                                            optimizer_idx = optimizer_idx,
-                                            batch_idx = batch_idx)
+        results = self.forward(real_img, labels=labels)
+        val_loss = self.model.loss_function(
+            *results,
+            M_N=1.0,  #real_img.shape[0]/ self.num_val_imgs,
+            optimizer_idx=optimizer_idx,
+            batch_idx=batch_idx)
 
-        self.log_dict({f"val_{key}": val.item() for key, val in val_loss.items()}, sync_dist=True)
+        self.log_dict(
+            {f"val_{key}": val.item()
+             for key, val in val_loss.items()},
+            sync_dist=True)
 
-        
     def on_validation_end(self) -> None:
         self.sample_images()
-        
+
     def sample_images(self):
-        # Get sample reconstruction image            
-        test_input, test_label = next(iter(self.trainer.datamodule.test_dataloader()))
+        # Get sample reconstruction image
+        test_input, test_label = next(
+            iter(self.trainer.datamodule.test_dataloader()))
         test_input = test_input.to(self.curr_device)
         test_label = test_label.to(self.curr_device)
 
-#         test_input, test_label = batch
-        recons = self.model.generate(test_input, labels = test_label)
-        vutils.save_image(recons.data,
-                          os.path.join(self.logger.log_dir , 
-                                       "Reconstructions", 
-                                       f"recons_{self.logger.name}_Epoch_{self.current_epoch}.png"),
-                          normalize=True,
-                          nrow=12)
+        #         test_input, test_label = batch
+        recons = self.model.generate(test_input, labels=test_label)
+        vutils.save_image(
+            recons.data,
+            os.path.join(
+                self.logger.log_dir, "Reconstructions",
+                f"recons_{self.logger.name}_Epoch_{self.current_epoch}.png"),
+            normalize=True,
+            nrow=12)
 
         try:
             samples = self.model.sample(144,
                                         self.curr_device,
-                                        labels = test_label)
-            vutils.save_image(samples.cpu().data,
-                              os.path.join(self.logger.log_dir , 
-                                           "Samples",      
-                                           f"{self.logger.name}_Epoch_{self.current_epoch}.png"),
-                              normalize=True,
-                              nrow=12)
+                                        labels=test_label)
+            vutils.save_image(
+                samples.cpu().data,
+                os.path.join(
+                    self.logger.log_dir, "Samples",
+                    f"{self.logger.name}_Epoch_{self.current_epoch}.png"),
+                normalize=True,
+                nrow=12)
         except Warning:
             pass
 
@@ -94,30 +117,29 @@ class VAEXperiment(pl.LightningModule):
         optims = []
         scheds = []
 
-        optimizer = optim.Adam(self.model.parameters(),
-                               lr=self.params['LR'],
-                               weight_decay=self.params['weight_decay'])
+        optimizer = get_optimizer(self.model.parameters(),
+                                  self.params['optimizer'])
         optims.append(optimizer)
         # Check if more than 1 optimizer is required (Used for adversarial training)
         try:
-            if self.params['LR_2'] is not None:
-                optimizer2 = optim.Adam(getattr(self.model,self.params['submodel']).parameters(),
-                                        lr=self.params['LR_2'])
+            if self.params['optimizer_2'] is not None:
+                optimizer2 = get_optimizer(
+                    getattr(self.model, self.params['submodel']).parameters(),
+                    self.params['optimizer_2'])
                 optims.append(optimizer2)
         except:
             pass
 
         try:
-            if self.params['scheduler_gamma'] is not None:
-                scheduler = optim.lr_scheduler.ExponentialLR(optims[0],
-                                                             gamma = self.params['scheduler_gamma'])
+            if self.params['lr_scheduler'] is not None:
+                scheduler = get_scheduler(optims[0],
+                                          self.params['lr_scheduler'])
                 scheds.append(scheduler)
-
                 # Check if another scheduler is required for the second optimizer
                 try:
-                    if self.params['scheduler_gamma_2'] is not None:
-                        scheduler2 = optim.lr_scheduler.ExponentialLR(optims[1],
-                                                                      gamma = self.params['scheduler_gamma_2'])
+                    if self.params['lr_scheduler_2'] is not None:
+                        scheduler2 = get_scheduler(
+                            optims[1], self.params['lr_scheduler_2'])
                         scheds.append(scheduler2)
                 except:
                     pass
